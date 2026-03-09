4b9371356a398d9579f73441cf618e49a8abfb90
Anatolii <anatoliivoronkinqa@gmail.com>
2026-03-09 14:37:41 +0100

    change BaseTest.js

--- tests/BaseTest.js
+++ tests/BaseTest.js
@@ -1,26 +1,30 @@
 const HomePage = require("../pages/HomePage.js");
 const AuthenticatedPage = require("../pages/book_store/AuthenticatedPage.js");
-const { createDriver, quitDriver }= require("../utils/DriverUtils.js");
+const { createDriver, quitDriver } = require("../utils/DriverUtils.js");
 const { saveScreenshot } = require("../utils/ScreenshotUtils.js");
+const { By, until } = require("selenium-webdriver");
 
 let driver;
 let url = 'https://demoqa.com/';
 let homePage;
 
 before(async function() {
-    // runs once before all tests
-    driver= await createDriver();
+    driver = await createDriver();
     if (process.env.CI !== 'true') {
         await driver.manage().window().maximize();
     }
-    this.driver=driver;
+    this.driver = driver;
 });
 
 beforeEach(async function() {
-    // runs before each test
     await driver.get(url);
-    homePage =new HomePage(driver);
-    this.homePage=homePage;
+    // wait for page to fully load before handing it to the test
+    await driver.wait(
+        until.elementLocated(By.css('.home-banner')),
+        process.env.CI === 'true' ? 30000 : 10000
+    );
+    homePage = new HomePage(driver);
+    this.homePage = homePage;
 });
 
 afterEach(async function () {
@@ -28,23 +32,20 @@ afterEach(async function () {
         await saveScreenshot(driver, this);
     }
 });
+
 afterEach(async function () {
-    //for the correct state before the next test after successful authorization
     if (this.currentTest.ctx.authSucceeded === true) {
-        const userPage= new AuthenticatedPage(driver);
+        const userPage = new AuthenticatedPage(driver);
         await userPage.clickLogoutButton();
     }
 });
+
 after(async function() {
-    // runs once after all tests
-    //await new Promise(r => setTimeout(r, 3000));
     await quitDriver();
 });
 
-/**
- * @returns {import('../pages/HomePage')}
- */
 function getHomePage() {
-  return homePage;
+    return homePage;
 }
+
 module.exports = { getHomePage };
